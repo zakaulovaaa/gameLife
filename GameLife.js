d32cd0d7d53d34ec4b7af58f11720adfc08e9edb
zakaulovaaa <darya.zakaulova@yandex.ru>
2021-01-08 01:50:37 +0800

    fix

---
+++ GameLife.js
@@ -0,0 +1,231 @@
+class GameLife {
+
+    getWidthBlockById(id) {
+        let block = document.getElementById(id);
+        if (block !== null) {
+            return block.offsetWidth;
+        }
+        return 0;
+    }
+
+    getHeightBlockById(id) {
+        let block = document.getElementById(id);
+        if (block !== null) {
+            return block.offsetHeight;
+        }
+        return 0;
+    }
+
+    setSizeCanvases() {
+        //если блок с настройками и канвас влезают в один скрин
+        if (window.innerWidth - this.settingsWidth >= this.minWidthCanvas) {
+            /** Установили количество ячеек */
+            let width = div(window.innerWidth - this.settingsWidth, this.cellSize);
+            width = width - 1 + width % 2;
+            this.inputWidth.value = width;
+
+            let height = div(window.innerHeight - this.titleHeight, this.cellSize);
+            height = height - 1 + height % 2;
+            this.inputHeight.value = height;
+
+            /** установили новый размер канвасов */
+            this.canvas.canvas.height = window.innerHeight - this.titleHeight;
+                // height * this.cellSize + 1;
+            this.canvas.canvas.width = window.innerWidth - this.settingsWidth; // width * this.cellSize + 1;
+
+            this.game.canvas.height = window.innerHeight - this.titleHeight;
+            //height * this.cellSize + 1;
+            this.game.canvas.width = window.innerWidth - this.settingsWidth; //width * this.cellSize + 1;
+
+        } else {
+            /** TODO: Написать правила для маленьких экранов */
+
+        }
+    }
+
+    constructor() {
+        /** Устанавливаем переменные */
+        this.settingsWidth = this.getWidthBlockById("block_settings"); //ширина блока с настройками -- для отступов
+        this.minWidthCanvas = 400;
+        this.titleHeight = this.getHeightBlockById("main_title");
+        this.cellSize = 7; //размер клетки
+        this.canvas = document.getElementById('back').getContext('2d');
+        this.game = document.getElementById('game').getContext('2d');
+        this.canvas.translate(0.5, 0.5);
+
+        this.inputWidth = document.getElementById('width');
+        this.inputHeight = document.getElementById('height');
+        this.inputTimeout = document.getElementById('timeout');
+
+        this.size = {x: this.inputWidth.value, y: this.inputHeight.value};
+
+        this.cells = [];
+        this.buffCells = [];
+
+        this.isAutoplay = false;
+
+        /** Выполняем основные функции */
+        this.setSizeCanvases();
+
+    }
+
+    gridFill() {
+        for (let i = 0; i < this.size.x; i++) {
+            this.cells[i] = [];
+            this.buffCells[i] = [];
+            for (let j = 0; j < this.size.y; j++) {
+                this.cells[i][j] = false;
+                this.buffCells[i][j] = false;
+            }
+        }
+    }
+    gridDraw() {
+        this.canvas.beginPath();
+        let widthCanvas = this.inputWidth.value * this.cellSize;
+        let heightCanvas = this.inputHeight.value * this.cellSize;
+
+        for (let i = 0; i <= this.size.y; i++) {
+            this.canvas.moveTo(0, i * this.cellSize + 0.5);
+            this.canvas.lineWidth = 1;
+            this.canvas.lineTo(widthCanvas, i * this.cellSize + 0.5);
+            this.canvas.strokeStyle = "rgb(50, 50, 50)"; // цвет линии
+        }
+
+        for (let i = 0; i <= this.size.x; i++) {
+            this.canvas.lineWidth = 1;
+            this.canvas.moveTo(i * this.cellSize + 0.5, 0);
+            this.canvas.lineTo(i * this.cellSize + 0.5, heightCanvas);
+            this.canvas.strokeStyle = "rgb(50, 50, 50)"; // цвет линии
+        }
+
+        this.canvas.stroke();
+
+        this.canvas.beginPath();
+        this.canvas.fillStyle = "#213b32";
+        this.canvas.fillRect(this.inputWidth.value * this.cellSize, 0, 10000, 10000);
+        this.canvas.fillRect(0, this.inputHeight.value * this.cellSize, 10000, 10000);
+
+        this.canvas.stroke();
+
+
+    }
+
+    clearGrid() {
+        this.game.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    fillCell(x, y) {
+        this.game.fillStyle = "rgb(150, 170, 150)";
+        this.game.fillRect(x * this.cellSize, y * this.cellSize, this.cellSize + 1, this.cellSize + 1);
+    }
+
+    fillGrid() {
+        this.clearGrid();
+        for (let i = 0; i < this.size.x; i++) {
+            for (let j = 0; j < this.size.y; j++) {
+                if (this.cells[i][j] === true) {
+                    this.fillCell(i, j);
+                }
+            }
+        }
+    }
+
+    randomFill() {
+        /** Заполняем матрицу */
+        for (let i = 0; i < this.size.x; i++) {
+            for (let j = 0; j < this.size.y; j++) {
+                let fill = Math.random() <= 0.5;
+                this.cells[i][j] = Boolean(fill);
+            }
+        }
+        /** Отрисовываем */
+        this.fillGrid();
+    }
+
+    getLivingNeighbors(x, y) {
+        let ans = 0;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) continue;
+                let cntX = x + i, cntY = y + j;
+
+                if (cntX < 0) cntX = this.size.x - 1;
+
+                if (cntX >= this.size.x) cntX = 0;
+
+                if (cntY < 0) cntY = this.size.y - 1;
+                if (cntY >= this.size.y) cntY = 0;
+
+                if (this.cells[cntX][cntY]) ans++;
+            }
+        }
+        return ans;
+    }
+
+    updateCells() {
+        for (let i = 0; i < this.size.x; i++) {
+            for (let j = 0; j < this.size.y; j++) {
+                let res = false, cnt = this.getLivingNeighbors(i, j);
+
+                if (this.cells[i][j] === true && cnt === 2) {
+                    res = true;
+                }
+                if (cnt === 3) {
+                    res = true;
+                }
+                this.buffCells[i][j] = res;
+            }
+        }
+
+        for (let i = 0; i < this.size.x; i += 1) {
+            for (let j = 0; j < this.size.y; j += 1) {
+                this.cells[i][j] = this.buffCells[i][j];
+            }
+        }
+
+        this.fillGrid();
+    }
+
+
+    autoplay() {
+        if (this.isAutoplay) {
+            this.updateCells();
+
+            setTimeout(() => {
+                this.autoplay();
+            }, this.inputTimeout.value);
+        }
+    }
+
+    initGame() {
+        /** очищаем поле */
+        this.canvas.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        /** устанавливаем размеры */
+        this.canvas.width = this.cellSize * this.inputWidth.value;
+        this.canvas.height = this.cellSize * this.inputHeight.value;
+
+        this.size = {x: this.inputWidth.value, y: this.inputHeight.value};
+
+        this.gridFill();
+        this.gridDraw();
+    }
+
+    downloadPng (filename) {
+        let canvas2 = document.getElementById('game')
+        let lnk = document.createElement('a'), e;
+        lnk.download = filename;
+        lnk.href = canvas2.toDataURL("image/png;base64");
+
+        if (document.createEvent) {
+            e = document.createEvent("MouseEvents");
+            e.initMouseEvent("click", true, true, window,
+                0, 0, 0, 0, 0, false, false, false,
+                false, 0, null);
+
+            lnk.dispatchEvent(e);
+        } else if (lnk.fireEvent) {
+            lnk.fireEvent("onclick");
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,431 +1,70 @@
 'use strict'; //ES5 строгий режим
-var console;
 
-let SETTINGS_WIDTH = 400;
-let MIN_WIDTH_CANVAS = 400;
-let TITLE_SIZE = 90;
+let game = new GameLife();
+game.initGame();
 
+let randBtn = document.getElementById('rand');
+randBtn.onclick = function () {
+    game.randomFill();
+};
 
-var CELL_SIZE = 7; //размер клетки
-var cells = [], buffCells = [];
-var timeout = 30; //задержка для автоплея
-var canvas, game;
-canvas = document.getElementById('back').getContext('2d');
-game = document.getElementById('game').getContext('2d');
-canvas.translate(0.5, 0.5);
+//Кнопка autoplay
+let autoplayBtn = document.getElementById('autoplay');
+autoplayBtn.onclick = function () {
+    game.isAutoplay = true;
+    game.autoplay();
+};
 
-var length = 1;
-var isAutoPlay = false
+let stopBtn = document.getElementById('btnStop');
+stopBtn.onclick = function () {
+    game.isAutoplay = false;
+};
 
-if (window.innerWidth - SETTINGS_WIDTH >= MIN_WIDTH_CANVAS) {
-    let width = div(window.innerWidth - SETTINGS_WIDTH, CELL_SIZE);
-    document.getElementById('width').value = width - (1 - width % 2);
+let stepBtn = document.getElementById('step');
+stepBtn.onclick = function () {
+    game.updateCells();
+};
 
-    let height = div(window.innerHeight - TITLE_SIZE, CELL_SIZE);
-    document.getElementById('height').value = height - (1 - height % 2);
+let clearBtn = document.getElementById('clear');
+clearBtn.onclick = function () {
+    game.isAutoplay = false;
+    game.clearGrid();
+    game.initGame();
+};
 
-    canvas.canvas.height = window.innerHeight - TITLE_SIZE + 2;
-    canvas.canvas.width = window.innerWidth - SETTINGS_WIDTH + 2;
+let saveBtn = document.getElementById('savePNG');
+saveBtn.onclick = function () {
+    game.downloadPng("file.png")
+};
 
-    game.canvas.height = window.innerHeight - TITLE_SIZE + 2;
-    game.canvas.width = window.innerWidth - SETTINGS_WIDTH + 2;
-
+let widthGrid = document.getElementById("width");
+widthGrid.onchange = () => {
+    game.isAutoplay = false;
+    game.clearGrid();
+    game.initGame();
 }
 
-function init() {
-
-
-    //back-grid
-    // console.log(window.innerWidth);
-    // console.log(window.innerHeight);
-
-    canvas.clearRect(0, 0, 1400, 1400)
-    // canvas.translate(0.5, 0.5);
-    canvas.width = CELL_SIZE * document.getElementById('width').value;
-    canvas.height = CELL_SIZE * document.getElementById('height').value;
-
-    //game
-
-
-    /* Сетка */
-    function Grid() {
-        this.size = {x: 0, y: 0};
-        this.width = canvas.width;
-        this.height = canvas.height;
-        // canvas.clear()
-        //grid functions
-
-
-        this.size.x = parseInt(canvas.width / CELL_SIZE, 10); //округляем к наименьшему
-        this.size.y = parseInt(canvas.height / CELL_SIZE, 10);
-
-        /* заполняем массив cells */
-        this.fill = function () {
-            var i, j;
-            for (i = 0; i < this.size.x; i += 1) {
-                cells[i] = [];
-                buffCells[i] = [];
-                for (j = 0; j < this.size.y; j += 1) {
-                    cells[i][j] = false; //false - нет жизни, true есть
-                    buffCells[i][j] = false;
-                }
-            }
-        };
-
-        var marginGor = (1000 - CELL_SIZE * document.getElementById('width').value) / 2;
-
-        /* рисуем сетку */
-        this.draw = function () {
-            var i;
-
-            // canvas.transform(1, 0, 0, -1, 0, canvas.height)
-            // canvas.translate(0.5, 0.5);
-            canvas.beginPath();
-
-            for (i = 0; i <= this.size.y; i += 1) {
-                canvas.moveTo(marginGor, i * CELL_SIZE);
-                canvas.lineWidth = 1;
-                canvas.lineTo(this.width + marginGor, i * CELL_SIZE);
-                canvas.strokeStyle = "rgb(80, 80, 80)"; // цвет линии
-            }
-
-            for (i = 0; i <= this.size.x; i += 1) {
-                canvas.lineWidth = 1;
-                canvas.moveTo(i * CELL_SIZE + marginGor, 0);
-                canvas.lineTo(i * CELL_SIZE + marginGor, canvas.height);
-                canvas.strokeStyle = "rgb(80, 80, 80)"; // цвет линии
-            }
-
-            canvas.stroke();
-        };
-    }
-
-
-    /* обновляем отрисовку */
-    function Update() {
-
-        /* Очистка ячеек */
-        this.clear = function () {
-            var marginGor = (1000 - CELL_SIZE * document.getElementById('width').value) / 2
-            game.clearRect(marginGor, 0, canvas.width, canvas.height);
-        };
-
-        /* Заполнить конкретную ячейку */
-        this.fillCell = function (x, y) {
-            var marginGor = (1000 - CELL_SIZE * document.getElementById('width').value) / 2
-            game.fillStyle = "rgb(150, 170, 150)";
-            // game.fillStyle="#f2c840";
-            game.fillRect(x * CELL_SIZE + marginGor, y * CELL_SIZE, CELL_SIZE + 1, CELL_SIZE + 1);
-        };
-
-        /* Заполнить всё поле */
-        this.fill = function () {
-            var i, j, grid = new Grid(), upd = new Update();
-
-            //Очищаем предыдущий кадр
-            upd.clear();
-
-            for (i = 0; i < grid.size.x; i += 1) {
-                for (j = 0; j < grid.size.y; j += 1) {
-                    //Тут можно устроить инверсию цвета
-                    if (cells[i][j] === true) {
-                        upd.fillCell(i, j);
-                    }
-                }
-            }
-            //Пересчитываем ячейки
-            upd.cells();
-        };
-
-        /* рандомная заливка для тестов */
-        this.randomFill = function () {
-            var i, j, fill, fillRnd, grid = new Grid(), upd = new Update();
-            //очищаем предыдущий рисунок
-            upd.clear();
-
-            for (i = 0; i < grid.size.x; i += 1) {
-                for (j = 0; j < grid.size.y; j += 1) {
-                    //рандомизация boolean
-                    fill = [true, false][Math.round(Math.random())];
-                    cells[i][j] = Boolean(fill);
-                }
-            }
-            for (i = 0; i < grid.size.x; i += 1) {
-                for (j = 0; j < grid.size.y; j += 1) {
-                    fill = cells[i][j];
-                    if (fill === true) {
-                        //заполняем новый рисунок
-                        fillRnd = new Update();
-                        fillRnd.fillCell(i, j);
-                    }
-                }
-            }
-        };
-
-        /* АВТОШАГ */
-        this.autoplay = function () {
-            if (isAutoPlay) {
-                var upd = new Update();
-                upd.fill();
-                timeout = document.getElementById('timeout').value
-                setTimeout(function () {
-                    upd.autoplay();
-                }, timeout);
-            }
-        };
-
-        /* Проверяем количество живых соседей */
-        this.getLivingNeighbors = function (x, y) {
-            var grid = new Grid(), count = 0, sx = grid.size.x, sy = grid.size.y;
-            //ПРАВИЛА ИГРЫ
-            //Проверяем верхнюю левую ячейку !
-            if (x !== 0 && y !== 0 && cells[x - 1][y - 1] === true) {
-                count += 1;
-            }
-            //Проверяем верхнюю ячейку !
-            if (y !== 0 && cells[x][y - 1] === true) {
-                count += 1;
-            }
-            //Проверяем верхнюю правую ячейку !
-            if (x !== sx - 1 && y !== 0 && cells[x + 1][y - 1] === true) {
-                count += 1;
-            }
-            //Проверяем левую ячейку !
-            if (x !== 0 && cells[x - 1][y] === true) {
-                count += 1;
-            }
-            //Проверяем правую ячейку !
-            if (x !== sx - 1 && cells[x + 1][y] === true) {
-                count += 1;
-            }
-            //Проверяем нижнюю левую ячейку !
-            if (x !== 0 && y !== sy - 1 && cells[x - 1][y + 1] === true) {
-                count += 1;
-            }
-            //Проверяем нижнюю ячейку !
-            if (y !== sy - 1 && cells[x][y + 1] === true) {
-                count += 1;
-            }
-            //Проверяем нижнюю правую ячейку !
-            if (x !== sx - 1 && y !== sy - 1 && cells[x + 1][y + 1] === true) {
-                count += 1;
-            }
-            //топология тор
-            if (x === 0 && cells[sx - 1][y] === true) {
-                count += 1
-            }
-            if (x === 0 && y !== sy - 1 && cells[sx - 1][y + 1] === true) {
-                count += 1
-            }
-            if (x === 0 && y !== 0 && cells[sx - 1][y - 1] === true) {
-                count += 1
-            }
-            if (x === sx - 1 && cells[0][y] === true) {
-                count += 1
-            }
-            if (x === sx - 1 && y !== sy - 1 && cells[0][y + 1] === true) {
-                count += 1
-            }
-            if (x === sx - 1 && y !== 0 && cells[0][y - 1] === true) {
-                count += 1
-            }
-            if (y === 0 && cells[x][sy - 1] === true) {
-                count += 1
-            }
-            if (y === 0 && x !== 0 && cells[x - 1][sy - 1] === true) {
-                count += 1
-            }
-            if (y === 0 && x !== sx - 1 && cells[x + 1][sy - 1] === true) {
-                count += 1
-            }
-            if (y === sy - 1 && cells[x][0] === true) {
-                count += 1
-            }
-            if (y === sy - 1 && x !== 0 && cells[x - 1][0] === true) {
-                count += 1
-            }
-            if (y === sy - 1 && x !== sx - 1 && cells[x + 1][0] === true) {
-                count += 1
-            }
-
-            return count;
-        };
-
-        /* Проверяем клетки по правилам игры */
-        this.cells = function () {
-            var i, j, isAlive, count, result = false, gameUpd = new Update(), grid = new Grid();
-            //сначала нужно скопировать массив
-            for (i = 0; i < grid.size.x; i += 1) {
-                for (j = 0; j < grid.size.y; j += 1) {
-                    result = false;
-                    //Проверяем состояние ячейки
-                    isAlive = cells[i][j];
-                    //считаем живых соседей
-                    count = gameUpd.getLivingNeighbors(i, j);
-                    //применяем правила
-                    if (isAlive && count < 2) {
-                        result = false;
-                    }
-                    if (isAlive && (count === 2 || count === 3)) {
-                        result = true;
-                    }
-                    if (isAlive && count > 3) {
-                        result = false;
-                    }
-                    if (!isAlive && count === 3) {
-                        result = true;
-                    }
-                    //записываем результат
-                    buffCells[i][j] = result;
-                }
-            }
-
-            //копируем массив. Сделаю через цикл, чтобы наверняка))0
-            for (i = 0; i < grid.size.x; i += 1) {
-                for (j = 0; j < grid.size.y; j += 1) {
-                    cells[i][j] = buffCells[i][j];
-                }
-            }
-        };
-
-    }
-
-    var gameGrid = new Grid(), gameUpd = new Update(), clearBtn, randBtn, stepBtn, stopBtn, saveBtn, saveField,
-        loadField, data;
-    gameGrid.draw();
-    gameGrid.fill();
-    //gameUpd.fillCell(10, 10);
-
-    //Кнопка очистки
-    clearBtn = document.getElementById('clear');
-    clearBtn.onclick = function () {
-        // alert('qqq')
-        isAutoPlay = false;
-        gameUpd.clear();
-    };
-
-    //Кнопка рандомизации
-    randBtn = document.getElementById('rand');
-    randBtn.onclick = function () {
-        gameUpd.randomFill();
-    };
-
-    //Кнопка шага
-    stepBtn = document.getElementById('step');
-    stepBtn.onclick = function () {
-        gameUpd.fill();
-    };
-
-    //Кнопка autoplay
-    stepBtn = document.getElementById('autoplay');
-    stepBtn.onclick = function () {
-        isAutoPlay = true;
-        var upd = new Update();
-        upd.autoplay();
-    };
-
-    //Остановить автоплей
-    stopBtn = document.getElementById('btnStop');
-    stopBtn.onclick = function () {
-        // alert('!');
-        isAutoPlay = false;
-    };
-
-    function download(filename) {
-        var canvas2 = document.getElementById('game')
-        var lnk = document.createElement('a'), e;
-        lnk.download = filename;
-        lnk.href = canvas2.toDataURL("image/png;base64");
-
-        if (document.createEvent) {
-            e = document.createEvent("MouseEvents");
-            e.initMouseEvent("click", true, true, window,
-                0, 0, 0, 0, 0, false, false, false,
-                false, 0, null);
-
-            lnk.dispatchEvent(e);
-        } else if (lnk.fireEvent) {
-            lnk.fireEvent("onclick");
-        }
-    }
-
-
-    saveBtn = document.getElementById('savePNG');
-    saveBtn.onclick = function () {
-        download("filename.png")
-    };
-
-    function getField() {
-        var ans = ""
-        ans += document.getElementById('height').value + " " + document.getElementById('width').value + "\n";
-
-        for (var i = 0; i < document.getElementById('height').value; i += 1) {
-            for (var j = 0; j < document.getElementById('width').value; j += 1) {
-                ans += cells[i][j] === true ? "1 " : "0 ";
-            }
-            ans += "\n";
-        }
-
-        return ans;
-    }
-
-    // saveField = document.getElementById('saveField');
-    // saveField.onclick = function () {
-    //     var text = getField();
-    //     let a = document.createElement("a");
-    //     let file = new Blob([text], {type: 'application/json'});
-    //     a.href = URL.createObjectURL(file);
-    //     a.download = "example.txt";
-    //     a.click();
-    // };
-
-    // loadField = document.getElementById('loadField');
-    // loadField.addEventListener("change", handleFiles, false);
-
-    function handleFiles() {
-        var file = this.files[0]; /* now you can work with the file list */
-        let reader = new FileReader();
-
-        reader.readAsText(file);
-
-
-        var fileLine;
-        reader.onload = function () {
-            fileLine = reader.result;
-            var mass = fileLine.split("\n")
-
-            document.getElementById('height').value = mass[0].split(" ")[0]
-            document.getElementById('width').value = mass[0].split(" ")[1]
-
-            init()
-            var i, j, fill, fillRnd, grid = new Grid(), upd = new Update();
-            //очищаем предыдущий рисунок
-            upd.clear();
-
-
-            for (var i = 1; i < document.getElementById('height').value; i++) {
-                var massi = mass[i].split(" ")
-                for (var j = 1; j < document.getElementById('width').value; j++) {
-                    cells[i][j] = massi[j] === "1" ? true : false
-                }
-            }
-
-            for (i = 0; i < grid.size.x; i += 1) {
-                for (j = 0; j < grid.size.y; j += 1) {
-                    fill = cells[i][j];
-                    if (fill === true) {
-                        //заполняем новый рисунок
-                        fillRnd = new Update();
-                        fillRnd.fillCell(i, j);
-                    }
-                }
-            }
-        };
-    }
+let heightGrid = document.getElementById("height");
+heightGrid.onchange = () => {
+    game.isAutoplay = false;
+    game.clearGrid();
+    game.initGame();
 }
 
-window.onload = init();
+// let cellSize = document.getElementById("cellSize");
+// cellSize.onchange = () => {
+//     game.isAutoplay = false;
+//     game.cellSize = cellSize.value;
+//     game.clearGrid();
+//     game.initGame();
+// }
 
+
+
+
+
+
+/** Для работы попапа с описанием игры */
 document.addEventListener("DOMContentLoaded", () => {
     let btn_open_description = document.getElementById("btn-open_description");
     let close_popup = document.getElementById("close_popup");
@@ -439,5 +78,4 @@ document.addEventListener("DOMContentLoaded", () => {
         popup.style.display = "none";
         fon.style.display = "none";
     }
-
-})
+});
